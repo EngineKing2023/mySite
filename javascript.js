7fe90710adc54f6ff7571290d80331379d7096bd
EngineKing2023 <engineking2023@gmail.com>
2025-02-08 22:36:50 +0100

    testing responsivnes and buttons

--- javascript.js
+++ javascript.js
@@ -1,5 +1,102 @@
 let myDocument = document.documentElement;
 let btn =document.getElementById("btn");
+let button1 =document.querySelector(".button1");
+let button2 =document.querySelector(".button2");
+let button3 =document.querySelector(".button3");
+let button4 =document.querySelector(".button4");
+let button5 =document.querySelector(".button5");
+let button6 =document.querySelector(".button6");
+
+let aboutUs = document.querySelector(".aboutUs");
+let isShowAboutUs = true;
+let contact = document.querySelector(".contact");
+let isShowContact = true;
+let engine = document.querySelector(".engine");
+let isShowEngine = true;
+let unity = document.querySelector(".unity");
+let isShowUnity = true;
+let websites = document.querySelector(".websites");
+let isShowWebsites = true;
+let plumber = document.querySelector(".plumber");
+let isShowPlumber = true;
+
+function showAboutUs(){
+    if(isShowAboutUs){
+        
+        aboutUs.style.display ="flex";
+        isShowAboutUs = false;
+    }
+}
+function closeAboutUs(){
+    if(closeAboutUs){
+        aboutUs.style.display ="none";
+        isShowAboutUs = true;
+    }
+}
+
+
+
+function showContact(){
+if(isShowContact){
+    contact.style.display ="block";
+    isShowContact = false;
+}
+    
+}
+function closeContact(){
+    if(closeContact){
+        contact.style.display ="none";
+        isShowContact = true;
+    }
+}
+function showEngine(){
+    if(isShowEngine){
+        engine.style.display ="block";
+        isShowEngine = false;
+    }
+}
+function closeEngine(){
+    if(closeEngine){
+        engine.style.display ="none";
+        isShowEngine = true;
+    }
+}
+function showUnity(){
+    if(isShowUnity){
+        unity.style.display ="block";
+        isShowUnity = false;
+    }
+}
+function closeUnity(){
+    if(closeUnity){
+        unity.style.display ="none";
+        isShowUnity = true;
+    }
+}
+function showWebsites(){
+    if(isShowWebsites){
+        websites.style.display ="block";
+        isShowWebsites = false;
+    }
+}
+function closeWebsites(){
+    if(closeWebsites){
+        websites.style.display ="none";
+        isShowWebsites = true;
+    }
+}
+function showPlumber(){
+    if(isShowPlumber){
+        plumber.style.display ="block";
+        isShowPlumber = false;
+    }
+}
+function closePlumber(){
+    if(closePlumber){
+        plumber.style.display ="none";
+        isShowPlumber = true;
+    }
+}
 
 btn.addEventListener("click",()=>{
     if(btn.textContent == "enter"){
@@ -20,9 +117,13 @@ btn.addEventListener("click",()=>{
 
 
 
-
-
         btn.textContent = "exit";
+        button1.style.display="block";
+        button2.style.display="block";
+        button3.style.display="block";
+        button4.style.display="block";
+        button5.style.display="block";
+        button6.style.display="block";
     }
     else{
             if(document.exitFullscreen){
@@ -41,8 +142,13 @@ btn.addEventListener("click",()=>{
 
 
 
-
-
-        btn.textContent = "enter";
+            btn.textContent = "enter";
+            button1.style.display="none";
+            button2.style.display="none";
+            button3.style.display="none";
+            button4.style.display="none";
+            button5.style.display="none";
+            button6.style.display="none";
+            
     }
 })
