c9070f6255e1a7f4b464c63cc076afadd9076aa9
EngineKing2023 <engineking2023@gmail.com>
2025-02-09 19:30:27 +0100

    media querry for phone finished

--- javascript.js
+++ javascript.js
@@ -20,7 +20,25 @@ let isShowWebsites = true;
 let plumber = document.querySelector(".plumber");
 let isShowPlumber = true;
 
+let slideIndex = 0;
+showSlides();
 
+function showSlides() {
+  let i;
+  let slides = document.getElementsByClassName("mySlides");
+  let dots = document.getElementsByClassName("dot");
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";  
+  }
+  slideIndex++;
+  if (slideIndex > slides.length) {slideIndex = 1}    
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex-1].style.display = "block";  
+  dots[slideIndex-1].className += " active";
+  setTimeout(showSlides, 2000); // Change image every 2 seconds
+}
 
 
 function showAboutUs(){
@@ -69,7 +87,7 @@ function closeEngine(){
 }
 function showUnity(){
     if(isShowUnity){
-        unity.style.display ="block";
+        unity.style.display ="flex";
         unity.style.position = "absolute";
         isShowUnity = false;
     }
@@ -82,7 +100,7 @@ function closeUnity(){
 }
 function showWebsites(){
     if(isShowWebsites){
-        websites.style.display ="block";
+        websites.style.display ="flex";
         websites.style.position = "absolute";
         isShowWebsites = false;
     }
@@ -95,7 +113,7 @@ function closeWebsites(){
 }
 function showPlumber(){
     if(isShowPlumber){
-        plumber.style.display ="block";
+        plumber.style.display ="flex";
         plumber.style.position = "absolute";
     }
 }
